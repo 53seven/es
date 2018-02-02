f6b6b2db12158c831938cc9ef445c3c86ad1f44f
Kiernan McGowan <timmcgowan625@gmail.com>
2018-02-02 12:51:06 -0600

    init

---
+++ es.js
@@ -0,0 +1,15 @@
+const url = require('url');
+const elasticsearch = require('elasticsearch');
+
+let config;
+if (process.env.ELASTICSEARCH_URL) {
+  const params = url.parse(process.env.ELASTICSEARCH_URL);
+  config = {
+    host: params.host,
+    httpAuth: params.auth
+  };
+}
+
+const client = new elasticsearch.Client(config);
+
+module.exports = client;
